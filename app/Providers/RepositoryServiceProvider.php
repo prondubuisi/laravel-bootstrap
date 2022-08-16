58856268259c88bd7d7a36d80c22022c163c0d65
Ndubuisi Onyemenam <onyemenamndu@gmail.com>
2022-08-16 10:15:11 +0100

    Add repository boostrap

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       // $this->app->bind();
+    }
+}
